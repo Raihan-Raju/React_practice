17f2b5e043c8b532c6503bad2a23b8660f6f821b
Raihan-Raju <idbraihan58@gmail.com>
2024-07-03 19:21:30 +0600

    Sadaka

---
+++ Fainal-project-of-React/project-sadaka/src/adminComponents/AdminNavbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AdminNavbar = () => {
+    return (
+        <div>
+            <h1>this is AdminNavbar</h1>
+        </div>
+    );
+};
+
+export default AdminNavbar;
---
+++ Fainal-project-of-React/project-sadaka/src/adminComponents/AdminSidebar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AdminSidebar = () => {
+    return (
+        <div>
+            this is sidebar of admin
+        </div>
+    );
+};
+
+export default AdminSidebar;
